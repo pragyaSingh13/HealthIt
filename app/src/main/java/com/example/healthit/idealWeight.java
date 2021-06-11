3d78e2fb4d85ab4770689c280d588a3572ba6573
Pragya Singh <pragyaprogrammer135@gmail.com>
2021-06-11 20:27:34 +0530

    fixed bugs in idealWeight amd made Gui changes

--- app/src/main/java/com/example/healthit/idealWeight.java
+++ app/src/main/java/com/example/healthit/idealWeight.java
@@ -23,11 +23,48 @@ public class idealWeight extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ideal_weight);
         button = findViewById(R.id.button4);
-        heighttext = findViewById(R.id.high);
+
         fbtn = findViewById(R.id.radioButton);
         mbtn = findViewById(R.id.radioButton4);
         tv  =findViewById(R.id.tv22);
         tvc = findViewById(R.id.textView12);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                heighttext = (EditText)findViewById(R.id.high);
+                float height = Float.parseFloat(heighttext.getText().toString());
+                float temp = 0;
+                if(fbtn.isChecked()){
+                    if(height<=60){
+                        tv.setText("49kg");
+                    }
+                    else if(height<48){
+                        Toast.makeText(idealWeight.this, "Please enter a valid adult height", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        tv.setText(""+(((height-60)*1.7)+49)+"kg");
+                    }
+
+                }
+                else if(mbtn.isChecked()){
+
+                    if(height<=60){
+                        tv.setText("50kg");
+                    }
+                    else if(height<48){
+                        tv.setText("Please enter a valid adult height");
+                    }
+                    else{
+                        tv.setText(""+(((height-60)*1.9)+50)+"kg");
+                    }
+                }
+                else{
+                    Toast.makeText(idealWeight.this, "Please select your gender first" +
+                            "", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
         tvc.setOnClickListener(new View.OnClickListener() {
