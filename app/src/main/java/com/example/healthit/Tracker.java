df99e30b4c898095096821b9aa351b25e5a351ce
Pragya Singh <pragyaprogrammer135@gmail.com>
2021-06-14 02:58:20 +0530

    Updated chart activity

--- app/src/main/java/com/example/healthit/Tracker.java
+++ app/src/main/java/com/example/healthit/Tracker.java
@@ -1,9 +1,12 @@
     package com.example.healthit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -19,12 +22,14 @@ import java.util.Map;
 public class Tracker extends AppCompatActivity  {
     private static final String TAG="Tracker";
     private LineChart mChart;
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
 
         mChart = findViewById(R.id.linechart);
+        imageButton = findViewById(R.id.imageButton);
        // mChart.setOnChartGestureListener(Tracker.this);
         //mChart.setOnChartValueSelectedListener(Tracker.this);
         mChart.setDragEnabled(true);
@@ -32,15 +37,15 @@ public class Tracker extends AppCompatActivity  {
         mChart.setBorderColor(android.R.color.white);
         mChart.setDrawGridBackground(false);
         mChart.setGridBackgroundColor(android.R.color.white);
-
-
-
-
         ArrayList<Entry> yValues= new ArrayList<>();
         yValues.add(new Entry(0,60f));
         yValues.add(new Entry(2,50f));
         yValues.add(new Entry(3,60f));
-        yValues.add(new Entry(4,70f));
+        yValues.add(new Entry(5,70f));
+        yValues.add(new Entry(6,30f));
+        yValues.add(new Entry(7,20f));
+        yValues.add(new Entry(8,50f));
+
 
 
         LineDataSet set1 = new LineDataSet(yValues,"set1");
@@ -55,6 +60,13 @@ public class Tracker extends AppCompatActivity  {
         dataSets.add(set1);
         LineData data =new LineData(dataSets);
         mChart.setData(data);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              new AlertDialog.Builder(getApplicationContext()).setTitle("Warning").setMessage("Are you sure you want to reset the chart?").show();
+            }
+        });
 
     }
 }
